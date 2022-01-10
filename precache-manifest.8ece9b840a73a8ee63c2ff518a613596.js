32baeed1f045d2e682dd0d4877ca345a4aa0a9b5
Neko Bar <nekobar0325@gmail.com>
2022-01-10 09:37:06 -0700

    Updates

---
+++ precache-manifest.8ece9b840a73a8ee63c2ff518a613596.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812729fd7407470ca0543c30cedd0c33",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "648aae52a5c56f20621b",
+    "url": "/web/static/css/main.f29c0a7e.chunk.css"
+  },
+  {
+    "revision": "0cbe43955381274179b7",
+    "url": "/web/static/js/2.6b9a4045.chunk.js"
+  },
+  {
+    "revision": "648aae52a5c56f20621b",
+    "url": "/web/static/js/main.ef39bbc1.chunk.js"
+  },
+  {
+    "revision": "33ed4ec846e9d84e8327",
+    "url": "/web/static/js/runtime~main.c75ffd02.js"
+  },
+  {
+    "revision": "e0baa4b03f3a02ccfb1d52b5d8b6156a",
+    "url": "/web/static/media/behind.e0baa4b0.webp"
+  },
+  {
+    "revision": "d7253385c9a2c033d50fd347643bdccc",
+    "url": "/web/static/media/facereco.d7253385.jpg"
+  },
+  {
+    "revision": "9ae6577d9ea94c5d6ea8b28678251f7a",
+    "url": "/web/static/media/fashion.9ae6577d.jpg"
+  },
+  {
+    "revision": "989eae97151efb5b752b217327931641",
+    "url": "/web/static/media/foodpic.989eae97.jpg"
+  },
+  {
+    "revision": "68fa355c7f7dfc85ea09bb5e0a6eeef1",
+    "url": "/web/static/media/front.68fa355c.webp"
+  },
+  {
+    "revision": "33ebf6acd5b83c5eef184220dd0fc848",
+    "url": "/web/static/media/kimchi.33ebf6ac.webp"
+  },
+  {
+    "revision": "9e93ea77d26ccd5e864ca68aa3f50834",
+    "url": "/web/static/media/meetup.9e93ea77.jpg"
+  },
+  {
+    "revision": "2438c7ee6c0d87575641a3b8e8dceecc",
+    "url": "/web/static/media/pet.2438c7ee.jpg"
+  },
+  {
+    "revision": "ec3d91c891ab2e6010863f3003273fcb",
+    "url": "/web/static/media/photography.ec3d91c8.jpg"
+  },
+  {
+    "revision": "6e985e1d56739551f553f1caf7eb6dd3",
+    "url": "/web/static/media/shopping.6e985e1d.jpg"
+  },
+  {
+    "revision": "318a9e2bf0e394ebfb7876004d7fe089",
+    "url": "/web/static/media/wed.318a9e2b.jpg"
+  }
+]);
