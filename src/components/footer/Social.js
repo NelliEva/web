02ea5b4cb4e09b7803bfff1f8992b0e375fb7fd6
NelliEva <55017487+NelliEva@users.noreply.github.com>
2022-04-03 08:00:29 -0400

    social

--- src/components/footer/Social.js
+++ src/components/footer/Social.js
@@ -6,9 +6,9 @@ function Social(){
         <div className='social'>
            <span className='logoSpan'><p>NelliEva</p></span>
            {/* <span> <SocialIcon url="https://facebook.com" bgColor="#e5a1a7" fgColor="white" /></span> */}
-           <span> <SocialIcon url="https://instagram.com/nellieva" bgColor="#e5a1a7" fgColor="white" /></span>
-           <span><SocialIcon url="https://github.com/NelliEva" bgColor="#e5a1a7" fgColor="white"/></span>
-           <span><SocialIcon url="https://linkedin.com/in/nellieva-szabo-100785224/" bgColor="#e5a1a7" fgColor="white" /></span>
+           <span className='insta'> <SocialIcon url="https://instagram.com/nellieva" bgColor="#e5a1a7" fgColor="white" /></span>
+           <span className='git'><SocialIcon url="https://github.com/NelliEva" bgColor="#e5a1a7" fgColor="white"/></span>
+           <span className='fb'><SocialIcon url="https://linkedin.com/in/nellieva-szabo-100785224/" bgColor="#e5a1a7" fgColor="white" /></span>
         </div>
     )
 }
